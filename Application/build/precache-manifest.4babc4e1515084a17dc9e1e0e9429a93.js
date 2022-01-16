7290fe3a6701d7278a89eda2a0bf4d5ad96a66ca
Arya Soni <aryasoni98@gmail.com>
2022-01-16 22:36:20 +0530

    Update

---
+++ Application/build/precache-manifest.4babc4e1515084a17dc9e1e0e9429a93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e1b5150833f6d7ab6fdfb03a5906694",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5a8f17c9d7775d0be74",
+    "url": "/static/css/main.a92348fe.chunk.css"
+  },
+  {
+    "revision": "af5eab6058baf0c66bc6",
+    "url": "/static/js/2.93f42b6e.chunk.js"
+  },
+  {
+    "revision": "d5a8f17c9d7775d0be74",
+    "url": "/static/js/main.94271c82.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
