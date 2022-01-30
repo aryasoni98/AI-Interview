1f25018ca360df8f3bc5ba151acb0a37964bf7f3
Prabhash Kumar <76423490+prabhashkumar11@users.noreply.github.com>
2022-01-30 20:20:43 +0530

    updated product hunt iframe icon

--- Application/src/components/navbar.component.jsx
+++ Application/src/components/navbar.component.jsx
@@ -48,6 +48,11 @@ const Navbar  = ()=>{
                                 </div>
                                 </a>
                             </div>
+                            <div className="d-flex align-items-center">
+                                <a href="https://www.producthunt.com/posts/ai-interview-system?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-ai-interview-system" target="_blank">
+                                <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=322838&theme=light" alt="AI Interview System - Test & Developed Your Interview Skills | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" />
+                                </a>
+                            </div>
                         </div>
                     </div>
                </nav>
